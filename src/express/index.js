13477a24537bc3de710e709f2b82e7935eea59be
aabdrahmanova <aaabdra5@gmail,com>
2020-05-09 16:40:08 +0300

    Учебный проект: шаблонизируй это

--- src/express/index.js
+++ src/express/index.js
@@ -1,14 +1,19 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 
 const offersRoutes = require(`./routes/offers-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 const app = express();
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.use(`/offers`, offersRoutes);
 app.use(`/my`, myRoutes);
--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -3,8 +3,8 @@
 const {Router} = require(`express`);
 const mainRouter = new Router();
 
-mainRouter.get(`/`, (req, res) => res.send(`/`));
-mainRouter.get(`/register`, (req, res) => res.send(`/register`));
-mainRouter.get(`/login`, (req, res) => res.send(`/login`));
+mainRouter.get(`/`, (req, res) => res.render(`main/main`));
+mainRouter.get(`/register`, (req, res) => res.render(`main/sign-up`));
+mainRouter.get(`/login`, (req, res) => res.render(`main/login`));
 
 module.exports = mainRouter;
