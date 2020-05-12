872d6132ca7f3006ee8637600e9bf00b0bcdee4f
aabdrahmanova <aaabdra5@gmail,com>
2020-05-12 13:22:01 +0300

    добавила обработку 404

--- src/express/index.js
+++ src/express/index.js
@@ -20,6 +20,12 @@ app.use(`/offers`, offersRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/`, mainRoutes);
 
+app.use((req, res) => {
+  res.status(404);
+  res.render(`errors/${res.statusCode}`);
+  console.error(chalk.red(`End request with error ${res.statusCode}`));
+});
+
 app.listen(DEFAULT_PORT, () => {
   console.info(chalk.green(`Сервер запущен на: http://localhost:${DEFAULT_PORT}`));
 });
