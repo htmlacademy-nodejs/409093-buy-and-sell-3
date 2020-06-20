02c4aa7e2b0e7ccf9992f6d2780f6b9d4dbc3331
aabdrahmanova <aaabdra5@gmail,com>
2020-06-20 12:35:35 +0300

    обновлен генератор моковых двнных

--- src/constants.js
+++ src/constants.js
@@ -5,10 +5,13 @@ module.exports = {
   USER_ARGV_INDEX: 2,
   DEFAULT_COUNT: 1,
   MAX_COUNT: 1000,
+  MAX_ID_LENGTH: 6,
+  MAX_COMMENTS: 10,
   FILE_NAME: `mocks.json`,
   FILE_SENTENCES_PATH: `./src/data/sentences.txt`,
   FILE_TITLES_PATH: `./src/data/titles.txt`,
   FILE_CATEGORIES_PATH: `./src/data/categories.txt`,
+  FILE_COMMENTS_PATH: `./src/data/comments.txt`,
   EXIT_CODE: {
     error: 1,
     success: 0,
--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -2,6 +2,7 @@
 
 const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
+const {nanoid} = require(`nanoid`);
 const {
   getRandomInt,
   shuffle,
@@ -9,11 +10,14 @@ const {
 const {
   DEFAULT_COUNT,
   MAX_COUNT,
+  MAX_ID_LENGTH,
+  MAX_COMMENTS,
   FILE_NAME,
   EXIT_CODE,
   FILE_SENTENCES_PATH,
   FILE_TITLES_PATH,
-  FILE_CATEGORIES_PATH
+  FILE_CATEGORIES_PATH,
+  FILE_COMMENTS_PATH
 } = require(`../../constants`);
 
 const {
@@ -36,14 +40,28 @@ const readContent = async (filePath) => {
   }
 };
 
-const generateAds = (count, titles, categories, sentences) => (
+const generateComments = (count, comments) => (
+  Array(count).fill({}).map(() => ({
+    id: nanoid(MAX_ID_LENGTH),
+    text: shuffle(comments)
+      .slice(0, getRandomInt(1, 3))
+      .join(` `),
+  }))
+);
+
+const generateAds = (count, titles, categories, sentences, comments) => (
   Array(count).fill().map(() => ({
-    category: shuffle(categories).slice(getRandomInt(0, categories.length - 1)),
-    description: shuffle(sentences).slice(1, 5).join(` `),
+    id: nanoid(MAX_ID_LENGTH),
+    category: shuffle(categories)
+            .slice(getRandomInt(0, categories.length - 1)),
+    description: shuffle(sentences)
+            .slice(1, 5)
+            .join(` `),
     picture: getPictureFileName(getRandomInt(PICTURE_NAME_MIN, PICTURE_NAME_MAX)),
     title: titles[getRandomInt(0, titles.length - 1)],
     type: Object.keys(AdType)[Math.floor(Math.random() * Object.keys(AdType).length)],
     sum: getRandomInt(AD_PRICE_MIN, AD_PRICE_MAX),
+    comments: generateComments(getRandomInt(1, MAX_COMMENTS), comments),
   }))
 );
 
@@ -51,6 +69,7 @@ const run = async (args) => {
   const sentences = await readContent(FILE_SENTENCES_PATH);
   const titles = await readContent(FILE_TITLES_PATH);
   const categories = await readContent(FILE_CATEGORIES_PATH);
+  const comments = await readContent(FILE_COMMENTS_PATH);
   const [count] = args;
   const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
 
@@ -58,7 +77,7 @@ const run = async (args) => {
     console.error(chalk.green(`Не больше 1000 объявлений`));
     process.exitCode = EXIT_CODE.success;
   } else if (titles.length && sentences.length && categories.length) {
-    const content = JSON.stringify(generateAds(countOffer, titles, categories, sentences));
+    const content = JSON.stringify(generateAds(countOffer, titles, categories, sentences, comments));
 
     try {
       await fs.writeFile(FILE_NAME, content);
